b25d49d049036f2a6bca1b099083612f297a79be
Bruno Cabral <bruno.cabral@amdocs.com>
2023-04-25 15:29:02 -0700

    compile windows

--- cmake/olcPGEConfig.cmake
+++ cmake/olcPGEConfig.cmake
@@ -2,15 +2,19 @@ cmake_minimum_required(VERSION 3.0)
 
 add_definitions(-DOLC_PGE_APPLICATION)
 add_definitions(-DOLC_PGEX_GRAPHICS2D)
-add_definitions(-DOLC_IMAGE_LIBPNG)
-add_definitions(-std=c++17)
 
 set(olcPGE_PREFIX       "${CMAKE_CURRENT_LIST_DIR}")
 set(olcPGE_INCLUDE_DIR  "${olcPGE_PREFIX}/..")
 
 include_directories(${olcPGE_INCLUDE_DIR})
 
-link_libraries(png GL X11 pthread stdc++fs)
+if(WIN32)
+    add_compile_options(/std:c++17)
+else()
+    add_definitions(-DOLC_IMAGE_LIBPNG)
+    add_compile_options(-std=c++17)
+    link_libraries(png GL X11 pthread stdc++fs)
+endif()
 
 get_directory_property( DirDefs DIRECTORY ${CMAKE_SOURCE_DIR} COMPILE_DEFINITIONS )
 
