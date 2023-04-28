3c224564d641d70d9fdbc93b83405564146dcd9e
Bruno Cabral <bruno.cabral@amdocs.com>
2023-04-27 18:05:26 -0700

    removed definitions from cmake

--- cmake/olcPGEConfig.cmake
+++ cmake/olcPGEConfig.cmake
@@ -1,9 +1,5 @@
 cmake_minimum_required(VERSION 3.0)
 
-add_definitions(-DOLC_PGE_APPLICATION)
-add_definitions(-DOLC_PGEX_GRAPHICS2D)
-add_definitions(-DOLC_PGEX_GRAPHICS3D)
-
 set(olcPGE_PREFIX       "${CMAKE_CURRENT_LIST_DIR}")
 set(olcPGE_INCLUDE_DIR  "${olcPGE_PREFIX}/..")
 
@@ -12,7 +8,6 @@ include_directories(${olcPGE_INCLUDE_DIR})
 if(WIN32)
     add_compile_options(/std:c++17)
 else()
-    add_definitions(-DOLC_IMAGE_LIBPNG)
     add_compile_options(-std=c++17)
     link_libraries(png GL X11 pthread stdc++fs)
 endif()
